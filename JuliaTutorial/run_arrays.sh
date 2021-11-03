9e302b4a7e9aa3df8cb8ea4fb3f0069f03031fe0
Seyoon Ko <kos@ucla.edu>
2021-11-03 10:35:30 -0700

    update simulations

---
+++ JuliaTutorial/run_arrays.sh
@@ -0,0 +1,4 @@
+qsub submit_array.sh # defaults to Normal()
+qsub submit_array.sh "Normal()"
+qsub submit_array.sh "TDist(5)"
+qsub submit_array.sh "TDist(1)"
---
+++ JuliaTutorial/submit_array.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#$ -cwd #uses current working directory
+# error = Merged with joblog
+#$ -o joblog.$JOB_ID.$TASK_ID #creates a file called joblog.jobidnumber.taskidnumber to write to. 
+#$ -j y 
+#$ -l h_rt=0:30:00,h_data=2G #requests 30 minutes, 2GB of data (per core)
+#$ -pe shared 2 #requests 2 cores
+# Email address to notify
+#$ -M $USER@mail #don't change this line, finds your email in the system 
+# Notify when
+#$ -m bea #sends you an email (b) when the job begins (e) when job ends (a) when job is aborted (error)
+#$ -t 100-500:100 # 100 to 500, with step size of 100
+
+# load the job environment:
+. /u/local/Modules/default/init/modules.sh
+module load julia/1.6 #loads julia/1.6 for use 
+
+echo ${SGE_TASK_ID}
+echo $1
+# run julia code
+echo 'Running runSim.jl for n = 100' #prints this quote to joblog.jobidnumber
+julia runSim.jl ${SGE_TASK_ID} 100 123 $1 > output.$JOB_ID.${SGE_TASK_ID} 2>&1 # n reps seed distribition
+
